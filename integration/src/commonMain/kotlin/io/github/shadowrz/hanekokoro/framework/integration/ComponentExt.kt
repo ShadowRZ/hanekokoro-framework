b308b583f565f60748c0dbf48e8125142e254c58
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2025-12-15 14:32:24 +0800

    fix: Fix childComponent in component creation

--- integration/src/commonMain/kotlin/io/github/shadowrz/hanekokoro/framework/integration/ComponentExt.kt
+++ integration/src/commonMain/kotlin/io/github/shadowrz/hanekokoro/framework/integration/ComponentExt.kt
@@ -2,8 +2,10 @@ package io.github.shadowrz.hanekokoro.framework.integration
 
 import com.arkivanov.decompose.ComponentContext
 import io.github.shadowrz.hanekokoro.framework.runtime.component.Component
+import io.github.shadowrz.hanekokoro.framework.runtime.plugin.Plugin
 
 inline fun <reified C : Component> Component.childComponent(
     context: ComponentContext,
+    plugins: List<Plugin> = emptyList(),
     hanekokoroApp: HanekokoroApp? = null,
-) = (hanekokoroApp ?: this.context.hanekokoroApp).component<C>(context = context)
+) = (hanekokoroApp ?: this.context.hanekokoroApp).component<C>(context = context, parent = this, plugins = plugins)
