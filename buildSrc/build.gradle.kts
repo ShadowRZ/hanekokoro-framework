edf59b672170309d4dc37476c1cce56af7719121
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-04-11 11:32:24 +0800

    chore(deps): update dokka to 2.2.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,5 +10,5 @@ repositories {
 dependencies {
     implementation("com.android.tools.build:gradle:9.1.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:2.1.0")
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:2.2.0")
 }
